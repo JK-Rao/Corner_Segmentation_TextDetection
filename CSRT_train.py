67d055522f62520220eb48e0b891f61e73f3dc18
root <jianminrao@163.com>
2018-08-06 12:33:46 +0800

    init training

---
+++ CSRT_train.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+# @Time      18-8-6 下午12:01
+# @File      CSRT_train.py
+# @Software  PyCharm
+# @Author    JK.Rao
+
+from libs.network.factory import get_network
+
+network=get_network('CSTR')
